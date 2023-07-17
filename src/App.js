c346e4589deb2131cf4de4ca0873df415f16e09c
Prashant Verma <prashantvermaiiitb@gmail.com>
2023-07-17 09:54:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {process.env.REACT_APP_NOT_SECRET_CODE}
         </a>
       </header>
     </div>
